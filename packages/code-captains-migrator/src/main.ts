504e1ddd644e7b0ac17382ef95b6399d4f9b25ce
Thomas Baker <thomas@titanops.co>
2024-10-18 12:09:19 -0400

    rework package json

--- packages/code-captains-migrator/src/main.ts
+++ packages/code-captains-migrator/src/main.ts
@@ -9,7 +9,7 @@ const main = async () => {
         .name("@upshift-dev/migrator")
         .description("A CLI to help migrate CODEOWNERS into captain files.")
         // todo: come up with clever way to keep this in sync with package.json
-        .version("0.1.1");
+        .version("0.1.2");
 
     program
         .command("migrate")
